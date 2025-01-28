5747dbcd8b0139936b0721f10daa43ac057d134e
Tanmyname <sultanbahri2304l@gmail.com>
2025-01-28 22:01:29 +0800

    Update pin.js

--- src/js/pin.js
+++ src/js/pin.js
@@ -16,7 +16,7 @@ async function pinSearch(query) {
     const response = await axios.get(url, {
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
-        'Cookie':  'Get your cookies from Pinterest ', //
+        'Cookie':  'Put your cookies that are obtained from the Pinterest web ', //
       },
     });
 

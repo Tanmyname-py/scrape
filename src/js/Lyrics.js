ffed94692c0b3b794dafd1c3569952c9cbf05fe6
Tanmyname <sultanbahri2304l@gmail.com>
2025-02-10 17:20:20 +0800

    Update Lyrics.js

--- src/js/Lyrics.js
+++ src/js/Lyrics.js
@@ -42,4 +42,30 @@ async function getLyricsLinks(songTitle) {
     }
 }
 
+async function getLyrics(url) {
+    try {
+        const headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+            "Connection": "keep-alive"
+        };
+        const { data } = await axios.get(url, { headers });
+        const $ = cheerio.load(data);
+        let title = $(".div-share h1").text().replace('Lirik lagu "', "").replace('"', "").trim() || "Unknown";
+        let artist = $(".lyricsh h2 a").text().trim() || "Unknown";
+        let lyrics = $(".ringtone").next("b").nextAll("div").first().html() || "";
+        lyrics = lyrics
+            .replace(/<br\s*\/?>/g, "\n")
+            .replace(/\n{2,}/g, "\n")
+            .replace(/<!--.*?-->/gs, "")
+            .trim();
 
+        return {
+            title: title,
+            artist: artist,
+            lyrics: lyrics
+        };
+
+    } catch (error) {
+        return { error: `❌ Gagal mengambil data: ${error.message}` };
+    }
+}

0312cfbdb7083e3d433cfc4729f2eeeaeb0c3647
Tanmyname <sultanbahri2304l@gmail.com>
2025-02-10 17:52:29 +0800

    Create Tenor.js
    Tenor scraper get gif

---
+++ src/js/Tenor.js
@@ -0,0 +1,29 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+async function getTenorGifs(query) {
+    try {
+        const searchUrl = `https://tenor.com/id/search/${encodeURIComponent(query)}-gifs`;
+        const headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+            "Connection": "keep-alive"
+        };
+        const { data } = await axios.get(searchUrl, { headers });
+        const $ = cheerio.load(data);
+
+        let results = [];
+
+        $(".UniversalGifListItem a div.Gif img").each((index, element) => {
+            let gifUrl = $(element).attr("src");
+            if (gifUrl) {
+                let title = gifUrl.split("/").pop().replace(".gif", "");
+                results.push({ title: title.replace(/-/g, " "), url: gifUrl });
+            }
+        });
+
+        return results;
+
+    } catch (error) {
+        return { error: `Gagal mengambil data: ${error.message}` };
+    }
+}
